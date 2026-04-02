b688f68acbdc70aa81cacd06ff8721a95aeb37ce
nana72848e <d1041211005@student.untan.ac.id>
2026-04-02 10:33:33 +0700

    fix netlify bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: "standalone" }),
+  adapter: netlify(),
   integrations: [vue()],
   vite: {
     plugins: [tailwindcss()],
